829a851a0a1f116c44a13d32692d21dc2e81a8f2
BigETI <bigeti.ethem@gmail.com>
2019-09-03 22:30:11 +0200

    Added functions to load translations from resources.

--- Scripts/Static/Translator.cs
+++ Scripts/Static/Translator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityTranslator.Objects;
 
 /// <summary>
 /// Unity translator namespace
@@ -48,6 +49,43 @@ namespace UnityTranslator
         {
             forcedSystemLanguage = SystemLanguage.English;
             forceSystemLanguage = false;
+        }
+
+        /// <summary>
+        /// Load from resource
+        /// </summary>
+        /// <param name="resourcePath">Resource path</param>
+        /// <returns>Translation if successful, otherwise "null"</returns>
+        public static TranslationObjectScript LoadFromResource(string resourcePath)
+        {
+            return Resources.Load<TranslationObjectScript>("Translations/" + resourcePath);
+        }
+
+        /// <summary>
+        /// Evaluate translatable string
+        /// </summary>
+        /// <param name="translatableString">Translatable string</param>
+        /// <returns>Translated string if successful, otherwise the input string if not null, otherwise an empty string</returns>
+        /// <remarks>
+        /// The input string will be evaluated, if if starts with the "{$" and ends with the "$}" suffix.
+        /// The inner part should reference a valid path within the "Translation" resource path.
+        /// </remarks>
+        public static string EvaluateTranslatableString(string translatableString)
+        {
+            string ret = ((translatableString == null) ? string.Empty : translatableString);
+            if (translatableString != null)
+            {
+                string trimmed_string = translatableString.Trim();
+                if ((trimmed_string.Length > 4) && trimmed_string.StartsWith("{$") && trimmed_string.EndsWith("$}"))
+                {
+                    TranslationObjectScript translation = LoadFromResource(trimmed_string.Substring(2, trimmed_string.Length - 4));
+                    if (translation != null)
+                    {
+                        ret = translation.ToString();
+                    }
+                }
+            }
+            return ret;
         }
     }
 }
